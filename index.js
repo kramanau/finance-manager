0f9154d7142db5a1dd099245bd428741bb785a11
Karolis Ramanauskas <kramanauskas112@gmail.com>
2024-05-20 20:40:54 -0700

    updated envvar and address of index

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const server = require("./server");
 const mongoose = require("mongoose");
 
 const port = process.env.PORT || 3000;
+const mongoURL = process.env.MONGO_CONNECTION_STRING || 'mongodb://127.0.0.1/temp';
 
 mongoose
-  .connect("mongodb://127.0.0.1/jscript-330-week-5", {})
+  .connect(mongoURL, {})
   .then(() => {
-    server.listen(port, () => {
+    server.listen(port, '0.0.0.0' ,() => {
       console.log(`Server is listening on http://localhost:${port}`);
     });
   })
